b0433a2916eb1f5b9023bb258099f05e1db997d7
karajanov <karajanovb@yahoo.com>
2020-02-24 23:19:42 +0100

    functionality part 1

--- AndroidCalculator/app/src/main/java/com/example/androidcalculator/MainActivity.java
+++ AndroidCalculator/app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -3,12 +3,82 @@ package com.example.androidcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button[] digits = new Button[10];
+    String firstOperand = new String("");
+    String secondOperand = new String("");
+    Button clearBtn;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.textViewResult);
+        clearBtn = findViewById(R.id.button_clear);
+
+        initializeDigitButtons(digits);
+        addClearFieldEvent(result);
+
+        for (int i = 0; i < digits.length; ++i) {
+            final int tempI = i;
+
+            digits[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    String digit = digits[tempI].getText().toString();
+                    String s = result.getText().toString();
+
+                    result.setText(s+digit);
+
+                }
+            });
+        }
+
+
+
     }
+
+
+    private int getDigitButtonId(int digit){
+
+        if(digit > 9 || digit < 0)
+            return -1;
+
+        int digitId = getResources()
+                .getIdentifier("button"+digit, "id", getPackageName());
+
+        return digitId;
+    }
+
+    private void initializeDigitButtons(Button[] digits) {
+
+        for (int i = 0; i < digits.length; ++i) {
+
+            int btnId = getDigitButtonId(i);
+            digits[i] = findViewById(btnId);
+
+        }
+    }
+
+    private void addClearFieldEvent(final TextView r){
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                r.setText("");
+            }
+        });
+    }
+
+
 }
