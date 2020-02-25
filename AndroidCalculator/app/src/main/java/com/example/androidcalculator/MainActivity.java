b9a64e0a6a488dcfa97e58ca52e58c8ad0e732a5
karajanov <karajanovb@yahoo.com>
2020-02-26 00:22:33 +0100

    functionality part 2

--- AndroidCalculator/app/src/main/java/com/example/androidcalculator/MainActivity.java
+++ AndroidCalculator/app/src/main/java/com/example/androidcalculator/MainActivity.java
@@ -11,21 +11,69 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Button[] digits = new Button[10];
-    String firstOperand = new String("");
-    String secondOperand = new String("");
-    Button clearBtn;
-    TextView result;
+    Button[] operators = new Button[4];
+    Button clearBtn, equalsBtn;
+    TextView result, tempResult;
+    String firstOperand = "";
+    String secondOperand = "";
+    String operator = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialization
         result = findViewById(R.id.textViewResult);
+        tempResult = findViewById(R.id.textViewTemp);
         clearBtn = findViewById(R.id.button_clear);
+        equalsBtn = findViewById(R.id.button_equals);
+        initializeDigitButtons();
+        initializeOperatorButtons();
 
-        initializeDigitButtons(digits);
-        addClearFieldEvent(result);
+        //Event listeners
+        addClearFieldsEvent();
+        addDigitEntryEvent();
+        addOperatorEvent();
+        addDisplayResultEvent();
+    }
+
+
+    private int getDigitButtonId(int digit) {
+
+        if (digit > 9 || digit < 0)
+            return -1;
+
+        int digitId = getResources()
+                .getIdentifier("button" + digit, "id", getPackageName());
+
+        return digitId;
+    }
+
+    private void initializeDigitButtons() {
+
+        for (int i = 0; i < digits.length; ++i) {
+
+            int btnId = getDigitButtonId(i);
+            digits[i] = findViewById(btnId);
+
+        }
+    }
+
+    private void initializeOperatorButtons() {
+
+        for (int i = 0; i < operators.length; ++i) {
+
+            int index = i + 10;
+            int opId = getResources()
+                    .getIdentifier("button" + index, "id", getPackageName());
+
+            operators[i] = findViewById(opId);
+
+        }
+    }
+
+    private void addDigitEntryEvent() {
 
         for (int i = 0; i < digits.length; ++i) {
             final int tempI = i;
@@ -36,49 +84,91 @@ public class MainActivity extends AppCompatActivity {
 
                     String digit = digits[tempI].getText().toString();
                     String s = result.getText().toString();
-
-                    result.setText(s+digit);
+                    String num = s + digit;
+                    result.setText(num);
 
                 }
             });
         }
-
-
-
     }
 
+    private void addOperatorEvent() {
 
-    private int getDigitButtonId(int digit){
+        for (int i = 0; i < operators.length; ++i) {
+            final int tempI = i;
 
-        if(digit > 9 || digit < 0)
-            return -1;
+            operators[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-        int digitId = getResources()
-                .getIdentifier("button"+digit, "id", getPackageName());
+                    operator = operators[tempI].getText().toString();
+                    firstOperand = result.getText().toString();
 
-        return digitId;
-    }
-
-    private void initializeDigitButtons(Button[] digits) {
-
-        for (int i = 0; i < digits.length; ++i) {
-
-            int btnId = getDigitButtonId(i);
-            digits[i] = findViewById(btnId);
+                    if(!firstOperand.isEmpty()) {
+                        tempResult.setText(firstOperand);
+                        result.setText("");
+                    }
+                }
+            });
 
         }
     }
 
-    private void addClearFieldEvent(final TextView r){
+    private void addClearFieldsEvent() {
 
         clearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                r.setText("");
+                result.setText("");
+                tempResult.setText("");
             }
         });
     }
 
+    private void addDisplayResultEvent(){
+
+        equalsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                firstOperand = tempResult.getText().toString();
+                secondOperand = result.getText().toString();
+
+                if (!(firstOperand.isEmpty()) && !(secondOperand.isEmpty())) {
+
+                    int a = Integer.parseInt(firstOperand);
+                    int b = Integer.parseInt(secondOperand);
+                    int c = 0;
+
+                    switch (operator) {
+
+                        case "+":
+                            c = a + b;
+                            break;
+
+                        case "-":
+                            c = a - b;
+                            break;
+
+                        case "*":
+                            c = a * b;
+                            break;
+
+                        case "/":
+                            c = (b == 0) ? ((int) Double.NaN) : (a / b);
+                            break;
+
+                        default:
+                            c = (int) Double.NaN;
+                            break;
+                    }
+
+                    tempResult.setText(String.valueOf(c));
+                }
+            }
+        });
+
+    }
 
 }
